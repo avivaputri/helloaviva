6a9a8f74aa7e6688f014cbdf36501bf0ed7c9d45
avivaputri <akunbarca69@gmail.com>
2020-06-25 14:01:04 +0700

    Updates

---
+++ precache-manifest.39c45f982cab8560d4daab2d8aa46d66.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c3fa0892159e0b1c534e1f5e73aab8",
+    "url": "/helloaviva/index.html"
+  },
+  {
+    "revision": "2497c4e5b4907fd24e7c",
+    "url": "/helloaviva/static/css/2.d41e5400.chunk.css"
+  },
+  {
+    "revision": "13a7d312582d3aba190f",
+    "url": "/helloaviva/static/css/main.cfb50218.chunk.css"
+  },
+  {
+    "revision": "2497c4e5b4907fd24e7c",
+    "url": "/helloaviva/static/js/2.67fc3511.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/helloaviva/static/js/2.67fc3511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a7d312582d3aba190f",
+    "url": "/helloaviva/static/js/main.4f7db273.chunk.js"
+  },
+  {
+    "revision": "885f58140d558cf2854e",
+    "url": "/helloaviva/static/js/runtime-main.f0579090.js"
+  },
+  {
+    "revision": "36e0fa231cef27456e6e6bb01e2ee8cc",
+    "url": "/helloaviva/static/media/afternoon.36e0fa23.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/helloaviva/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/helloaviva/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "53cefd5f53fa6b7132ea929fe0f67f59",
+    "url": "/helloaviva/static/media/bg.53cefd5f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/helloaviva/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/helloaviva/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/helloaviva/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/helloaviva/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/helloaviva/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloaviva/static/media/logo.5d5d9eef.svg"
+  }
+]);
